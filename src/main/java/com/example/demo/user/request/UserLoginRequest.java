f156bacfb23b52ea107ebb3d214c2246057f43a8
Choi Tae gyu <suker800@gmail.com>
2022-11-16 11:48:20 +0900

    사용자 회원가입 dto

---
+++ src/main/java/com/example/demo/user/request/UserLoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.user.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class UserLoginRequest {
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+}
